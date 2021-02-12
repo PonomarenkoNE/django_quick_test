2c861c943c3caffff1b6a8deb442c155e44b649d
HukuTa <ponom.nick@gmail.com>
2021-02-12 14:36:45 +0200

    init commit

--- django_app/myfirstapp/urls.py
+++ django_app/myfirstapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('task1', views.task1, name='task1'),
+    path('prodreport', views.task1, name='task1'),
     path('prodreport/<_id>/', views.prodreport, name='prodreport'),
 ]
--- django_app/myfirstapp/views.py
+++ django_app/myfirstapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import ProductionConsumed, Production
+from django.db.models import Sum
+from .models import ProductionConsumed, Production, ProductionProduced, Purchase
 
 def home(request):
     return render(request, 'index.html')
@@ -10,5 +11,18 @@ def task1(request):
     return render(request, 'task1.html', {'obj': obj})
 
 def prodreport(request, _id):
-    obj = Production.objects.get(id=_id)
-    return render(request, 'prodreport.html', {'obj': obj})
+    obj = Production.objects.filter(id=_id)
+    purchase = Purchase.objects.all()
+    price_dict = dict()
+    for el in purchase:
+        price_dict.update(
+            {el.material.name:
+                 Purchase.objects.filter(material=el.material).aggregate(Sum('price_ex_vat'))['price_ex_vat__sum']}
+        )
+    full_price = Purchase.objects.all().aggregate(Sum('price_ex_vat'))['price_ex_vat__sum']
+    produced = ProductionProduced.objects.filter(prod_report__in=obj)
+    consumed = ProductionConsumed.objects.filter(produced_consumed__in=produced)
+    return render(request, 'prodreport.html', {'obj': obj, 'produced': produced,
+                                               'consumed': consumed,'full_price': full_price,
+                                               'purchase': purchase, 'price_dict': price_dict})
+
